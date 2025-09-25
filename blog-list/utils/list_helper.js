d103c6e1d17a6af810d7d4a40179583553b92be5
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-09-25 12:44:17 +0200

    helper functions and unit tests, step 2

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -1,8 +1,17 @@
 const dummy = (blogs) => {
-	// ...
 	return 1;
+};
+
+const totalLikes = (blogs) => {
+	const initialValue = 0;
+	const reducer = (acc, currentValue) => {
+		acc += currentValue.likes;
+		return acc;
+	};
+	return blogs.reduce(reducer, initialValue);
 };
 
 module.exports = {
 	dummy,
+	totalLikes,
 };
