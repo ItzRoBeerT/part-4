d7721b3e05641791408483787ae684e54980d785
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-09-25 12:17:50 +0200

    blog list step 2, refactor

---
+++ blog-list/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const config = require('./utils/config');
+const mongoose = require('mongoose');
+const blogRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+
+const app = express();
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
