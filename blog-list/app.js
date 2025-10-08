704bf3507082618470c597c2d980d60ab5380e88
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-10-08 16:09:11 +0200

    Blog list expansion, step 3

--- blog-list/app.js
+++ blog-list/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const config = require('./utils/config');
 const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/user');
 const middleware = require('./utils/middleware');
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
